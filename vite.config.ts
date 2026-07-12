ac1b085496c5a9727ab4785a59e48168efc14003
Harshit Srivastava <Harshitsrivastava0914@gmail.com>
2026-07-12 15:06:13 +0530

    Vite file

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/hash-ai/' : '/',
+  base: command === 'build' ? '/hash-ai-main/' : '/',
+  
   server: {
     watch: {
       usePolling: true,
